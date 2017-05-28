f1df5607d2cfc9d2257474b63fd12c30c1699860
Lalit Mishra <lalit.mishra5@gmail.com>
2017-05-28 18:55:33 +0530

    added new project

---
+++ filesearch/src/com/lalit/filesearch/FindFile.java
@@ -0,0 +1,24 @@
+package com.lalit.filesearch;
+
+import java.io.File;
+import java.util.List;
+
+public class FindFile {
+
+	public void readFileFromFolders(File folder){
+		System.out.println(folder.getAbsoluteFile());
+		if(folder.isDirectory()){
+			String[] subFolder = folder.list();
+			for(String fileName: subFolder){
+				this.readFileFromFolders(new File(folder,fileName));
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       FindFile file = new FindFile();
+       file.readFileFromFolders(new File("C:\\Images"));
+	}
+
+}
